76a2f7360e530504d0c9a8e706e0ed964183d35d
Lhomas7 <landon7@clemson.edu>
2023-10-18 19:46:48 -0400

    giur3nvek

--- lab06.py
+++ lab06.py
@@ -16,6 +16,7 @@ deck = [
     "AC", "2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "10C", "JC", "QC", "KC",
     "AS", "2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "10S", "JS", "QS", "KS"
 ]
+"""select the number of players to play with and validate"""
 import random
 print("Give me a seed:")
 seed = int(input())
@@ -25,6 +26,7 @@ while num_players <= 0:
     print("Please enter a value greater than 0: ")
     num_players = int(input())
 
+"""define how to deal the deck of cards to each player"""
 player_hands = []
 def deal(num_players):
     for i in range(num_players):
@@ -45,36 +47,66 @@ def deal(num_players):
         while len(seen) != 1:
             print(f"Acknowledge that you have seen your cards player {i + 1} by entering any key.")
             seen = input().split()
-player_points = []
+
+"""define how to check if an Ace is 1 or 11"""
+def check_ace(count, points):
+    for hand in player_hands:
+        for card in hand:
+            if card == 'AH':
+                count += 1
+            elif card == 'AS':
+                count += 1
+            elif card == 'AD':
+                count += 1
+            elif card == 'AC':
+                count += 1
+        if count == 4 and (21 - points) >=14:
+            points += 14
+        elif count == 4 and (21 - points) < 14:
+            points += 4
+        elif count == 3 and (21 - points) >=13:
+            points += 13
+        elif count == 3 and (21 - points) < 13: 
+            points += 3
+        elif count == 2 and (21- points) >= 12:
+            points += 12
+        elif count == 2 and (21 - points) < 12:
+            points += 2
+        elif count == 1 and (21 - points) >= 11:
+            points += 11
+        elif count == 1 and (21 - points) < 11:
+            points += 1
+
+"""define how to get points for each player"""           
+player_points =[0 for i in range(num_players)]
 def points(player_hands):
     for i in range(num_players):
         points = 0
         for j in range(len(player_hands[i])):
-            if player_hands[i][j] == deck[9] or player_hands[i][j] == deck[10] or player_hands[i][j] == deck[11] or player_hands[i][j] == deck[12] or player_hands[i][j] == deck[22]  or player_hands[i][j] == deck[23] or player_hands[i][j] == deck[24] or player_hands[i][j] == deck[25] or player_hands[i][j] == deck[35] or player_hands[i][j] == deck[36] or player_hands[i][j] == deck[37] or player_hands[i][j] == deck[38] or player_hands[i][j] == deck[48] or player_hands[i][j] == deck[49] or player_hands[i][j] == deck[50] or player_hands[i][j] == deck[51]:
-                points += 10
-            elif player_hands[i][j] == deck[8] or player_hands[i][j] == deck[21] or player_hands[i][j] == deck[34] or player_hands[i][j] == deck[47]:
-                points += 9
-            elif player_hands[i][j] == deck[7] or player_hands[i][j] == deck[20] or player_hands[i][j] == deck[33] or player_hands[i][j] == deck[46]:
-                points += 8
-            elif player_hands[i][j] == deck[6] or player_hands[i][j] == deck[19] or player_hands[i][j] == deck[32] or player_hands[i][j] == deck[45]:
-                points += 7
-            elif player_hands[i][j] == deck[5] or player_hands[i][j] == deck[18] or player_hands[i][j] == deck[31] or player_hands[i][j] == deck[44]:
-                points += 6
-            elif player_hands[i][j] == deck[4] or player_hands[i][j] == deck[17] or player_hands[i][j] == deck[30] or player_hands[i][j] == deck[43]:
-                points += 5
-            elif player_hands[i][j] == deck[3] or player_hands[i][j] == deck[16] or player_hands[i][j] == deck[29] or player_hands[i][j] == deck[42]:
-                points += 4
-            elif player_hands[i][j] == deck[2] or player_hands[i][j] == deck[15] or player_hands[i][j] == deck[28] or player_hands[i][j] == deck[41]:
-                points += 3
-            elif player_hands[i][j] == deck[1] or player_hands[i][j] == deck[14] or player_hands[i][j] == deck[27] or player_hands[i][j] == deck[40]:
-                points += 2
-            elif player_hands[i][j] == deck[0] or player_hands[i][j] == deck[13] or player_hands[i][j] == deck[26] or player_hands[i][j] == deck[39]:
-                if (21 - points) >= 11:
-                    points += 11
-                else:
-                    points += 1
-        player_points.append(points)
+            if player_hands[i][j] != deck[0] and player_hands[i][j] != deck[13] and player_hands[i][j] != deck[26] and player_hands[i][j] != deck[39]:
+                if player_hands[i][j] == deck[9] or player_hands[i][j] == deck[10] or player_hands[i][j] == deck[11] or player_hands[i][j] == deck[12] or player_hands[i][j] == deck[22] or player_hands[i][j] == deck[23] or player_hands[i][j] == deck[24] or player_hands[i][j] == deck[25] or player_hands[i][j] == deck[35] or player_hands[i][j] == deck[36] or player_hands[i][j] == deck[37] or player_hands[i][j] == deck[38] or player_hands[i][j] == deck[48] or player_hands[i][j] == deck[49] or player_hands[i][j] == deck[50] or player_hands[i][j] == deck[51]:
+                    points += 10
+                elif player_hands[i][j] == deck[8] or player_hands[i][j] == deck[21] or player_hands[i][j] == deck[34] or player_hands[i][j] == deck[47]:
+                    points += 9
+                elif player_hands[i][j] == deck[7] or player_hands[i][j] == deck[20] or player_hands[i][j] == deck[33] or player_hands[i][j] == deck[46]:
+                    points += 8
+                elif player_hands[i][j] == deck[6] or player_hands[i][j] == deck[19] or player_hands[i][j] == deck[32] or player_hands[i][j] == deck[45]:
+                    points += 7
+                elif player_hands[i][j] == deck[5] or player_hands[i][j] == deck[18] or player_hands[i][j] == deck[31] or player_hands[i][j] == deck[44]:
+                    points += 6
+                elif player_hands[i][j] == deck[4] or player_hands[i][j] == deck[17] or player_hands[i][j] == deck[30] or player_hands[i][j] == deck[43]:
+                    points += 5
+                elif player_hands[i][j] == deck[3] or player_hands[i][j] == deck[16] or player_hands[i][j] == deck[29] or player_hands[i][j] == deck[42]:
+                    points += 4
+                elif player_hands[i][j] == deck[2] or player_hands[i][j] == deck[15] or player_hands[i][j] == deck[28] or player_hands[i][j] == deck[41]:
+                    points += 3
+                elif player_hands[i][j] == deck[1] or player_hands[i][j] == deck[14] or player_hands[i][j] == deck[27] or player_hands[i][j] == deck[40]:
+                    points += 2
+            else:
+                check_ace(0, points)
+        player_points[i] = points
 
+"""define how to add a card to a player's hand"""
 def add_card(player):
     new_card_num = random.randint(0, 51)
     new_card = deck[new_card_num]
@@ -84,7 +116,7 @@ def add_card(player):
         if i == player:
             player_hands[i].append(new_card)
 
-            
+"""begin the actual game"""            
 deal(num_players)
 print("Now that everyone knows their cards, let's play!")
 
@@ -100,12 +132,15 @@ for i in range(num_players):
         add_card(i)
         points(player_hands)
         if player_points[i] > 21:
+            print(player_hands[i])
             print(f"Player {i + 1} you have busted. Enter any key to acknowlege this.")
             busted = input().split()
             while len(busted) != 1:
                 print(f"Player {i + 1} you have busted. Enter any key to acknowlege this.")
-                busted = input().split()
-            player_hands[i] = 0       
+                busted = input().split()   
+            player_hands[i] = [0,0]
+            player_points[i] = 0
+            break 
         else:
             print(f"Player {i + 1}'s cards:")
             print(player_hands[i])
@@ -121,14 +156,16 @@ for i in range(num_players):
     if player_hands[i] == 0:       
         print(f"Player {i + 1} has busted.")
 
+"""determine winner"""
 max_points = max(player_points)
-if player_points.count(max_points) == 2:
-    winner1 = player_points.index(max_points)
-    winner2 = player_points.index(max_points, winner1 + 1)
-    print(f"Players {winner1 + 1} and {winner2 + 1} tied for the highest score of {max_points}")
-elif player_points.count(max_points) == 1:
-    winner = player_points.index(max_points)
-    print(f"Player {winner + 1} got the highest score of {max_points}.")
+if max_points > 0:     
+    if player_points.count(max_points) == 2:
+        winner1 = player_points.index(max_points)
+        winner2 = player_points.index(max_points, winner1 + 1)
+        print(f"Players {winner1 + 1} and {winner2 + 1} tied for the highest score of {max_points}")
+    elif player_points.count(max_points) == 1:
+        winner = player_points.index(max_points)
+        print(f"Player {winner + 1} got the highest score of {max_points}.")
 else:
     print("Nobody won.")
 
